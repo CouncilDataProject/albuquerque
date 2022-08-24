d9b2cfd8638a7ae96bafeda74a92768125058ce6
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-08-24 00:47:15 -0700

    Ignore some minutes items

--- python/cdp_albuquerque_backend/scraper.py
+++ python/cdp_albuquerque_backend/scraper.py
@@ -18,6 +18,20 @@ def get_events(
     scraper = LegistarScraper(
         client="cabq",
         timezone="America/Denver",
+        ignore_minutes_item_patterns=[
+            "Please refer to the statement",
+            "The Council will take a dinner break at approximately",
+            "To provide virtual public comments to the Council everyone",
+            "Members of the public will have the ability to view the meeting",
+            "The Council will take General Public Comment on any topic",
+            "Following Center for Disease Control and Prevention",
+            "To provide public comments to the Council",
+            "Public Comment Sign Up for",
+            "we are encouraging members of the public that attend the meeting",
+            "by completing this web form",
+            "Attention: Following Center for Disease Control and Prevention",
+            "For members of the public without internet service",
+        ],
     )
 
     return scraper.get_events(begin=from_dt, end=to_dt)
